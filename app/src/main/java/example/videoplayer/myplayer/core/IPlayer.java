4689d43608c18a818f21e841ec188953de582f4c
Wayne <liuzhy520@gmail.com>
2016-10-03 00:39:06 +0800

    add some classes

---
+++ app/src/main/java/example/videoplayer/myplayer/core/IPlayer.java
@@ -0,0 +1,8 @@
+package example.videoplayer.myplayer.core;
+
+/**
+ * Created by Wayne on 2016/10/3.
+ */
+
+public interface IPlayer {
+}
---
+++ app/src/main/java/example/videoplayer/myplayer/impl/MyVideoPlayer.java
@@ -0,0 +1,8 @@
+package example.videoplayer.myplayer.impl;
+
+/**
+ * Created by Wayne on 2016/10/3.
+ */
+
+public class MyVideoPlayer {
+}

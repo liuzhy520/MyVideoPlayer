de31e47c1270a4deb2bf6e4854f2637e0e62b8ef
Wayne <liuzhy520@gmail.com>
2016-10-14 10:34:18 +0800

    continue coding

--- myvideoplayer/src/main/java/com/liuzhy520/myvideoplayer/entity/MediaPlayerEntity.java
+++ myvideoplayer/src/main/java/com/liuzhy520/myvideoplayer/entity/MediaPlayerEntity.java
@@ -1,8 +1,75 @@
 package com.liuzhy520.myvideoplayer.entity;
 
+import android.media.MediaPlayer;
+
+import com.liuzhy520.myvideoplayer.core.IPlayer;
+
 /**
  * Created by Wayne on 2016/10/5.
+ * using {@link android.media.MediaPlayer} to play videos in here
+ * it can preload the next video in the list
  */
 
-public class MediaPlayerEntity {
+public class MediaPlayerEntity implements IPlayer,
+        MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void seekTo(int position) {
+
+    }
+
+    @Override
+    public int getCurrentVideoPosition() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentVideoDuration() {
+        return 0;
+    }
+
+    @Override
+    public boolean isVideoPlaying() {
+        return false;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
 }
--- myvideoplayer/src/main/java/com/liuzhy520/myvideoplayer/entity/VLCPlayerEntity.java
+++ myvideoplayer/src/main/java/com/liuzhy520/myvideoplayer/entity/VLCPlayerEntity.java
@@ -1,8 +1,50 @@
 package com.liuzhy520.myvideoplayer.entity;
 
+import com.liuzhy520.myvideoplayer.core.IPlayer;
+
 /**
  * Created by Wayne on 2016/10/5.
+ * this one would use the VLC Player
  */
 
-public class VLCPlayerEntity {
+public class VLCPlayerEntity implements IPlayer{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void seekTo(int position) {
+
+    }
+
+    @Override
+    public int getCurrentVideoPosition() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentVideoDuration() {
+        return 0;
+    }
+
+    @Override
+    public boolean isVideoPlaying() {
+        return false;
+    }
 }

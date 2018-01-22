6aef8d8d10393b23689bda8e57f4122ca24d977f
Wayne <liuzhy520@gmail.com>
2018-01-22 15:16:01 +0800

    change the launcher icon

---
+++ app/src/main/java/liuzhy520/videoplayer/HomeActivity.java
@@ -0,0 +1,9 @@
+package liuzhy520.videoplayer;
+
+/**
+ * Created by wayne on 1/22/18.
+ * Modified by
+ */
+
+public class HomeActivity {
+}
---
+++ app/src/main/java/liuzhy520/videoplayer/base/BaseActivity.java
@@ -0,0 +1,26 @@
+package liuzhy520.videoplayer.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wayne on 1/22/18.
+ * Modified by Wayne
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
